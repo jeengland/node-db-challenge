745e6dfe7631b49603d574474eaf88ebbca3338b
unknown <jacob.e.england@gmail.com>
2020-04-17 10:21:59 -0500

    Added migration to create tables

---
+++ data/migrations/20200417101117_create_tables.js
@@ -0,0 +1,49 @@
+
+exports.up = function(knex) {
+    return knex.schema 
+        .createTable('projects', tbl => {
+            tbl.increments();
+            tbl.string('name').unique().notNullable();
+            tbl.string('description');
+            tbl.boolean('completed').defaultTo(0);
+        })
+        .createTable('resources', tbl => {
+            tbl.increments();
+            tbl.string('name').unique().notNullable();
+            tbl.string('description').notNullable();
+        })
+        .createTable('project_resources', tbl => {
+            tbl.increments();
+            tbl
+                .integer('project_id')
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE');
+            tbl
+                .integer('resource_id')
+                .notNullable()
+                .references('id')
+                .inTable('resources')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE');
+        })
+        .createTable('tasks', tbl => {
+            tbl.increments();
+            tbl.string('description').notNullable();
+            tbl.string('notes');
+            tbl.boolean('completed').defaultTo(0);
+            tbl
+                .integer('project_id')
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE')
+        })
+};
+
+exports.down = function(knex) {
+
+};
